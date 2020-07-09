f58612d01f849ab4d3968c1ce84375ace78848af
sirDemian <sirdemian@gmail.com>
2020-07-09 19:05:09 -0300

    se agrega funcion para hacer el arbol filogenetico desde un fasta validado

--- src/geolocalizer/toolkits/AlignAndTree.py
+++ src/geolocalizer/toolkits/AlignAndTree.py
@@ -1,7 +1,7 @@
 from subprocess import *
 from datetime import datetime
 
-class Salchichon:
+class AlignAndTree:
 
     def generate_ouput_name(self, input_path, final_text='align'):
         now = datetime.now()
@@ -17,11 +17,16 @@ class Salchichon:
     def tree_from_align(self, input_path):
         output = self.generate_ouput_name(input_path, "tree")
         command_stdout = Popen(['clustalo', '-i', input_path, f'--guidetree-out={output}'], stdout=PIPE).communicate()[0]
+        return output[18:]
+
+    def tree_from_fasta(self, input_path):
+        output = self.tree_from_align(self.align_fasta(input_path))
         return output
+"""
+CASO DE USO
+
+salchicha = AlignAndTree()
+print(salchicha.tree_from_fasta('fasta_nombre[Especie]*Ciudad*Pais'))
+"""
 
 
-
-salchicha = Salchichon()
-temp = salchicha.align_fasta('fasta_nombre[Especie]*Ciudad*Pais')
-print(temp)
-print(salchicha.tree_from_align(temp))
