1dc4bcf29b9039989245b9c6e1ca808b55e0c5e9
sirDemian <sirdemian@gmail.com>
2020-07-12 23:18:35 -0300

    se agrega busqueda de geos por nombre de ciudad o pais

--- src/geolocalizer/alignAndTree.py
+++ src/geolocalizer/alignAndTree.py
@@ -16,15 +16,17 @@ class AlignAndTree:
         ).communicate()[0]
         return output
 
-    def tree_from_align(self, input_path):
+    def tree_from_align(self, input_path, bootstrap='1000', merge=True):
         output = self.generate_ouput_name(input_path, "tree")
-        command_stdout = Popen(
-            ["clustalo", "-i", input_path, f"--guidetree-out={output}"], stdout=PIPE
-        ).communicate()[0]
-        return output
+        if merge:
+            command_stdout = Popen(["iqtree", "-s", input_path, '-m', 'MFP+MERGE', '-bb', bootstrap], stdout=PIPE).communicate()[0]
+        else:
+            command_stdout = Popen(["iqtree", "-s", input_path, '-bb', bootstrap], stdout=PIPE).communicate()[0]
+        return output[:18]
 
 
 tree = AlignAndTree()
-temp = tree.align_fasta("fasta_nombre[Especie]*Ciudad*Pais")
+temp = tree.align_fasta("pruebaFasta_iqtree")
 print(temp)
 print(tree.tree_from_align(temp))
+
