ff961ec89038b3e176ddf15e8f6a649c4fece349
facundo <facundovigo@gmail.com>
2020-07-19 13:42:47 -0300

    codigo de main funcionado

--- main.py
+++ main.py
@@ -6,29 +6,20 @@ import sys, getopt
 
 
 if __name__ == "__main__":
-    email = ""
-    if not email:
-        raise Exception("Set Entrez email")
-
-    parsed_fasta = Parser().parse(
-        "tests/sequences/geolocalized_seqs.fasta", write_output=True
-    )
-    if (sys.argv[1].__str__ == "h") | (len(sys.argv) != 3):
+    
+    email = f'rizziromanalejandro@gmail.com'
+    if (sys.argv[1].__str__ == "h") or (len(sys.argv) != 3):
         print('case of use:   test.py file_align table_accessions')
     else:
         try:
             parsed_fasta = Parser().parse(
-                f'src/files/{sys.argv[1]}', write_output=True
+                "tests/sequences/geolocalized_seqs.fasta", write_output=True
             )
-            geo_services = GeoServices()
-            countries = geo_services.get_countries_from_xls(
-                f'src/files/{sys.argv[2]}'
-            )
+           
             geo_services = GeoServices(email)
-            geo_seqs = geo_services.get_location_for_idseq(parsed_fasta["seqs"])
+            geo_seqs = geo_services.geolocalize_seqs(parsed_fasta["seqs"])
             align_and_tree = AlignAndTree()
             output_path = align_and_tree.align_fasta(parsed_fasta["output_path"])
             tree_path = align_and_tree.tree_from_align(output_path)
-            Canvas(geo_seqs, tree_path).create_map_and_save_to("tmp/mapa_prueba.html")
         except getopt.GetoptError:
             print('test.py file_align table_accessions')
